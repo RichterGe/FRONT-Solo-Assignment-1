ecee5175bc1bbb880edaf9356f72b1bb57fa9374
Georg <ct26m016@technikum.wien.at>
2026-09-20 14:36:14 +0200

    Initial commit with working basic counter component

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
+</script>
+
+<template>
+  <Counter />
+</template>
---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import { computed } from 'vue'
+
+const count = ref(0)
+const doubled = computed(() => count.value*2)
+// const doubled = ref(2)
+
+function increment () {
+    count.value++;
+}
+
+function decrement () {
+    count.value--;
+}
+
+const incrementThatIsSomehowBetter = () => {
+    count++;
+}
+
+</script>
+
+<template>
+     <h1>Test num {{ count }}, double: {{ doubled }}</h1>
+     <p>
+        <button @click="increment()">Up</button>
+        <button @click="decrement()">Down</button>
+    </p>
+</template>
